3d43e8e6d70eafb5ecfbad78a823705836c70aae
Son Mai <sonmai@powergate.com>
2018-06-25 13:11:23 +0700

    Change structor project

--- the-complete-guide/src/app/app.module.ts
+++ the-complete-guide/src/app/app.module.ts
@@ -1,32 +1,36 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-import { HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { HeaderComponent } from './header/header-component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+
+import { RecipesComponent } from './Shopping-course-project/recipes/recipes.component';
+import { RecipeListComponent } from './Shopping-course-project/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './Shopping-course-project/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './Shopping-course-project/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './Shopping-course-project/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './Shopping-course-project/shopping-list/shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './Shopping-course-project/header/header.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule  
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
