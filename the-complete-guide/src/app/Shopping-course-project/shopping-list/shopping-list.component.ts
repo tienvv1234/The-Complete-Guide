d7d39e3d7e313bf0c743ce2a442987a813d310a6
Son Mai <sonmai@powergate.com>
2018-06-25 16:52:41 +0700

    Show list data for product create form edit

--- the-complete-guide/src/app/Shopping-course-project/shopping-list/shopping-list.component.ts
+++ the-complete-guide/src/app/Shopping-course-project/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 10),
+    new Ingredient("Tomatoes", 10)
+  ];
   constructor() { }
 
   ngOnInit() {
